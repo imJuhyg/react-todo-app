fa0cab6779131a7db876e0ba879771491f1c56ea
Juhyeong-Lim <limjuhyg@gmail.com>
2023-01-12 22:01:22 +0900

    Form Component, Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import React, {Component} from "react"; // "react"라이브러리에서 React와 Component 클래스를 가져온다.
 import React, { useState } from 'react';
 import "./App.css";
+import Form from './components/Form';
 import List from './components/List';
 
 export default function App() {
@@ -8,24 +9,8 @@ export default function App() {
   //   todoData : [],
   //   value : ""
   // };
-  const [todoData, setTodoData] = useState([
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    }
-  ]); // todoData state 정의
-  const [value, setValue] = useState(""); // value state 정의
-
-  const handleChange = (e) => {
-    // this.setState({ value : e.target.value });
-    setValue(e.target.value);
-  };
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault(); // 원래 submit은 페이지가 리로드된다. 이 것을 막아준다.
@@ -33,16 +18,12 @@ export default function App() {
     // 새로운 할 일 데이터
     let newTodo = {
       id: Date.now(),
-      // title: this.state.value,
       title: value,
       completed: false
     };
 
     // todoData에 새로운 할 일 추가하기
-    // this.setState({ todoData: [...todoData, newTodo] });
-    setTodoData(prev => [...prev, newTodo]); // setter에서 이전 state를 가져오기 위해서는 인수에 함수를 이용해서 가져올 수 있다.
-    // input에 있던 value 삭제
-    // this.setState({ value : ""} );
+    setTodoData((prev) => [...prev, newTodo]); // setter에서 이전 state를 가져오기 위해서는 인수에 함수를 이용해서 가져올 수 있다.
     setValue("");
   };
 
@@ -52,29 +33,11 @@ export default function App() {
     <div className="container">
       <div className="todoBlock">
         <div class="title">
-          <h1>할 일 목록</h1>
+          <h1 className="text-3xl font-bold">할 일 목록</h1>
         </div>
-
-        <List todoData={ todoData } setTodoData={ setTodoData }/>
-
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: '10', padding: '5px' }}
-            placeholder="해야할 일 을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: '1' }}
-          />
-        </form>
-
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+        <List todoData={todoData} setTodoData={setTodoData} />
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
   );
