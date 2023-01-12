0423d6c507f7cf240ed8f495faa61211a57f2c13
Juhyeong-Lim <limjuhyg@gmail.com>
2023-01-12 22:15:07 +0900

    /

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
         <div class="title">
           <h1 className="text-3xl font-bold">할 일 목록</h1>
         </div>
-        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+        <h1 className="text-3xl font-bold underline">Hello World!</h1> 
         <List todoData={todoData} setTodoData={setTodoData} />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
